a23af6b1a9150fe2904c75a347e3361d2cac2931
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-11-22 19:47:11 +0600

    write and read file asynchoronously

--- NodeJS/built-in-module/file-system/index.js
+++ NodeJS/built-in-module/file-system/index.js
@@ -5,8 +5,21 @@ const fs = require("fs");
 
 //read file synchronously
 const readFileText = fs.readFileSync("./file/readFile.txt", "utf-8");
-console.log(readFileText);
+// console.log(readFileText);
 
 //write file synchronously
 const writeFileData = "My Name is Yasin Khan Rabbi.I'm from Bangladesh";
 fs.writeFileSync("./file/writeFile.txt", writeFileData, "utf-8");
+
+// asynchronously ----
+//read file
+
+fs.readFile("./file/readFile.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+
+  const writeText = "My name is Yasin Khan Rabbi" + data;
+  fs.writeFile("./file/writeFileTwo.txt", writeText, "utf-8", (err) => {
+    if (err) throw err;
+  });
+});
